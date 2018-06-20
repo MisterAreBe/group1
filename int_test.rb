47176709e712fa712d83d67f88d4144010d3306d
Ricky Collins <richardcollins52317@gmail.com>
2018-06-20 09:56:24 -0400

    first

---
+++ int_test.rb
@@ -0,0 +1,13 @@
+def str(int1, int2)
+    if int1 == int2
+        puts true
+    else
+        puts false
+    end
+end
+str("1", 1)
+str(3.2, 2)
+str(1, 2)
+str(1, [])
+str(2,4)
+str("hello", "hello")
