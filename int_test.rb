e039d4279adaab002f3e5514a5257b7cbea12f27
Ricky Collins <richardcollins52317@gmail.com>
2018-06-20 10:21:38 -0400

    comparing classes

--- int_test.rb
+++ int_test.rb
@@ -1,13 +1,21 @@
 def str(int1, int2)
-    if int1 == int2
+    if int1.class == int2.class
         puts true
+        if int1 == int2
+            puts true; puts "\n"
+        else
+            puts false
+        end
     else
-        puts false
+        puts "\n"; puts false
     end
+    
 end
-str("1", 1)
-str(3.2, 2)
-str(1, 2)
-str(1, [])
-str(2,4)
+
+# str("1", 1)
+# str(3.2, 2)
+# str(1, 2)
+# str(1, [])
 str("hello", "hello")
+str("hello","hi")
+str(2, [2, 3, 4])
